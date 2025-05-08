9fcc3060870fe970751b3d6d6652addfb3c46357
Manuela <wanuvr.20@gmail.com>
2025-05-08 00:28:17 -0500

    Primera1

--- Money_Manager/src/App.jsx
+++ Money_Manager/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import {
+  MyRoutes,
+  Sidebar,
+  Device,
+  Light,
+  Dark,
+  AuthContextProvider,
+  Menuambur,
+  useUsuariosStore,
+  Login,
+  SpinnerLoader,
+  Fondo1,
+} from "./index";
+import { useLocation } from "react-router-dom";
+import { createContext, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { styled } from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+export const ThemeContext = createContext(null);
 function App() {
-  const [count, setCount] = useState(0)
+  const { mostrarUsuarios, datausuarios } = useUsuariosStore();
+
+  const { pathname } = useLocation();
+  // const [theme, setTheme] = useState("dark");
+  const theme = datausuarios?.tema === "0" ? "light" : "dark";
+  const themeStyle = theme === "light" ? Light : Dark;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { isLoading, error } = useQuery({queryKey:["mostrar usuarios"],queryFn: () =>
+    mostrarUsuarios()}
+  );
+
+  if (isLoading) {
+    return <SpinnerLoader />;
+  }
+  if (error) {
+    return <h1>Error..</h1>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <ThemeContext.Provider value={{ theme }}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            {pathname != "/login" ? (
+              <Container className={sidebarOpen ? "active" : ""}>
+                <div className="ContentSidebar">
+                  <Sidebar
+                    state={sidebarOpen}
+                    setState={() => setSidebarOpen(!sidebarOpen)}
+                  />
+                </div>
+                <div className="ContentMenuambur">
+                  <Menuambur />
+                </div>
 
-export default App
+                <Containerbody>
+                  <MyRoutes />
+                </Containerbody>
+              </Container>
+            ) : (
+              <Login />
+            )}
+
+            <ReactQueryDevtools initialIsOpen={true} />
+          </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </>
+  );
+}
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  background: ${({ theme }) => theme.bgtotal};
+  transition: all 0.2s ease-in-out;
+
+  .ContentSidebar {
+    display: none;
+  }
+  .ContentMenuambur {
+    display: block;
+    position: absolute;
+    left: 20px;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 65px 1fr;
+    &.active {
+      grid-template-columns: 220px 1fr;
+    }
+    .ContentSidebar {
+      display: initial;
+    }
+    .ContentMenuambur {
+      display: none;
+    }
+  }
+`;
+const Containerbody = styled.div`
+  grid-column: 1;
+  width: 100%;
+  @media ${Device.tablet} {
+    grid-column: 2;
+  }
+`;
+export default App;
